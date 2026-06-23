ee760dc9b02fed24e16423a0ca38c69f87fdb057
Jhon Limones <joarome95@gmail.com>
2026-06-23 09:27:59 +0200

    feat: envolvemos la app con el proveedor en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+// src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { AuthProvider } from './context/AuthContext'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        {/* AuthProvider envuelve toda la app para que el estado de auth este disponible en todos los componentes */}
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    </StrictMode>
 )
