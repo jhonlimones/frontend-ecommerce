da8de7a3f462338bd729695437fc46f453983c12
Jhon Limones <joarome95@gmail.com>
2026-06-23 10:45:45 +0200

    feat: añadimos importacion faltante del router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 // src/main.jsx
+import { BrowserRouter } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext'
