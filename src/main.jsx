ea6e3f3f15d5d90a5c95b2547f7d315f44e917dc
Jhon Limones <joarome95@gmail.com>
2026-06-23 09:39:39 +0200

    feat: añadimos el nuevo proveedor al main.jsx CarritoContext

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext'
+import { CarritoProvider } from './context/CarritoContext'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        {/* AuthProvider envuelve toda la app para que el estado de auth este disponible en todos los componentes */}
-        <AuthProvider>
-            <App />
-        </AuthProvider>
+        {/* BrowserRouter habilita la navegacion sin recarga de pagina */}
+        <BrowserRouter>
+            {/* AuthProvider debe ir primero porque CarritoProvider lo necesita */}
+            <AuthProvider>
+                {/* CarritoProvider envuelve la app para que el carrito este disponible en todos los componentes */}
+                <CarritoProvider>
+                    <App />
+                </CarritoProvider>
+            </AuthProvider>
+        </BrowserRouter>
     </StrictMode>
 )
